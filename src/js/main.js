6ce0e43d37d6742e82f30293cebd3f780026b34c
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2023-09-28 19:27:53 +0300

    add draft splide config

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
 import * as bootstrap from 'bootstrap'
+
+if (document.querySelector('.splide')) {
+    let splide = new Splide(".splide", {
+      type: "loop",
+      focus: 0,
+      gap: "1rem",
+      perPage: 4,
+      breakpoints: {
+        640: {
+          perPage: 2,
+        },
+        480: {
+          perPage: 1,
+        },
+      },
+    });
+  
+    splide.mount();
+  }
